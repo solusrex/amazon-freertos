9e7ee602cbc350ff120b3301cce4325d656862ec
Archit Aggarwal <architag@amazon.com>
2020-06-02 20:57:55 +0000

    Revert "Test with RC implementation of TimespecAdd"
    This reverts commit dd7d0b279867f6f1e6eac0d04e2986894aa180e1.

--- libraries/freertos_plus/standard/freertos_plus_posix/source/FreeRTOS_POSIX_utils.c
+++ libraries/freertos_plus/standard/freertos_plus_posix/source/FreeRTOS_POSIX_utils.c
@@ -186,38 +186,60 @@ int UTILS_TimespecAdd( const struct timespec * const x,
                        const struct timespec * const y,
                        struct timespec * const pxResult )
 {
-    int64_t llPartialSec = 0;
+    uint64_t ullPartialSec = 0;
     int iStatus = 0;
 
-    /* Check parameters. */
-    if( ( pxResult == NULL ) || ( x == NULL ) || ( y == NULL ) )
+    /* Check input parameters. None of the parameters should be NULL,
+     * and the input parameters values should be positive. */
+    if( ( pxResult == NULL ) || ( x == NULL ) || ( y == NULL ) ||
+        ( x->tv_sec < 0 ) || ( x->tv_nsec < 0 ) ||
+        ( y->tv_sec < 0 ) || ( y->tv_nsec < 0 ) )
     {
         iStatus = -1;
     }
 
+    configPRINTF( ( "Input tv_sec values: x->tv_sec: %u, y->tv_sec:%u",
+                    x->tv_sec, y->tv_sec ) );
+
     if( iStatus == 0 )
     {
-        /* Perform addition. */
+        /* Perform addition for the nanoseconds member. */
         pxResult->tv_nsec = x->tv_nsec + y->tv_nsec;
 
-        /* check for overflow in case nsec value was invalid */
-        if( pxResult->tv_nsec < 0 )
+        configPRINTF( ( "Values after nsec addition: tv_nsec=%ld, tv_sec=%d\n",
+                        pxResult->tv_nsec, pxResult->tv_sec ) );
+
+        /* Check if the addition resulted in an overflow.*/
+        if( ( pxResult->tv_nsec < x->tv_nsec ) || ( pxResult->tv_nsec < y->tv_nsec ) )
         {
             iStatus = 1;
+            configPRINTF( ( "Detected overflow in tv_nsec: ReturnStatus=%d", iStatus ) );
         }
         else
         {
-            llPartialSec = ( pxResult->tv_nsec ) / NANOSECONDS_PER_SECOND;
+            /* Prune the nanoseconds value to be less than NANOSECONDS_PER_SECOND. */
+            ullPartialSec = ( pxResult->tv_nsec ) / NANOSECONDS_PER_SECOND;
             pxResult->tv_nsec = ( pxResult->tv_nsec ) % NANOSECONDS_PER_SECOND;
-            pxResult->tv_sec = x->tv_sec + y->tv_sec + llPartialSec;
 
-            /* check for overflow */
-            if( pxResult->tv_sec < 0 )
+            /* Perform addition for the seconds member. */
+            pxResult->tv_sec = x->tv_sec + y->tv_sec + ullPartialSec;
+
+            configPRINTF( ( "Values after sec addition: tv_nsec=%ld, tv_sec=%d\n",
+                            pxResult->tv_nsec, pxResult->tv_sec ) );
+
+            /* Check if addition resulted in an overflow. */
+            if( ( pxResult->tv_sec < x->tv_sec ) || ( pxResult->tv_sec < y->tv_sec ) ||
+                ( pxResult->tv_sec < ullPartialSec ) )
             {
+                /* configPRINT_STRING( ( "Execution determines tv_sec is < 0\n" ) ); */
+
                 iStatus = 1;
+                configPRINTF( ( "Detected overflow in tv_sec: ReturnStatus=%d", iStatus ) );
             }
         }
     }
+
+    configPRINTF( ( "Return Status at end of function: %d\n", iStatus ) );
 
     return iStatus;
 }
